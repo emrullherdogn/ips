4547db5d44f2bd4849c19f833226d89c30efbaa5
emrullherdogn <emrullherdogn@gmail.com>
2025-04-06 23:59:08 +0300

    Add files via upload

---
+++ xdp_prog.c
@@ -0,0 +1,28 @@
+#include <linux/bpf.h>
+#include <linux/if_ether.h>
+#include <linux/ip.h>
+#include <bpf/bpf_helpers.h>
+
+char _license[] SEC("license") = "GPL";
+
+SEC("xdp")
+int xdp_simple(struct xdp_md *ctx) {
+    void *data = (void *)(long)ctx->data;
+    void *data_end = (void *)(long)ctx->data_end;
+
+    struct ethhdr *eth = data;
+    if ((void *)(eth + 1) > data_end)
+        return XDP_PASS;
+
+    // Sadece IP paketlerini kontrol ediyoruz
+    if (eth->h_proto != __constant_htons(ETH_P_IP))
+        return XDP_PASS;
+
+    struct iphdr *ip = (void *)(eth + 1);
+    if ((void *)(ip + 1) > data_end)
+        return XDP_PASS;
+
+    // Daha fazla kontrol yok: sadece IP paketlerini geçiyoruz
+    return XDP_PASS;
+}
+
